180bfb01e11282d1c2aa5c8718468b9c1644e2cf
Josh Nunn <screenbeard@users.noreply.github.com>
2019-03-12 01:39:30 +1030

    Split JS into files and updated to Hugo 0.5X to use templates in JS

---
+++ assets/js/app.js
@@ -0,0 +1,85 @@
+(function() {
+    'use strict';
+
+    {{ if or (.Site.Params.showTagCloud | default true) (.Site.Params.showtoc | default true) (.Site.Params.showScrollToTop | default true) }}
+
+    window.addEventListener("load", function(){
+        {{ if .Site.Params.showScrollToTop | default true }}
+        document.getElementById("scroll-to-top").onclick = function(){
+                document.body.scrollTop = 0; // For Safari
+                document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        };
+        {{ end }}
+
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+
+            {{ if .Site.Params.showTagCloud | default true }}
+            if (document.getElementById("tag-cloud") !== null) {
+                    if (prevScrollpos > currentScrollPos) { // scroll up
+                            document.getElementById("tag-cloud").style.visibility = "visible";
+                            document.getElementById("tag-cloud").style.opacity = "1";
+                    } else {
+                            document.getElementById("tag-cloud").style.visibility = "hidden";
+                            document.getElementById("tag-cloud").style.opacity = "0";
+                    }
+            }
+            {{ end }}
+
+            {{ if .Site.Params.showScrollToTop | default true }}
+            if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+                document.getElementById("scroll-to-top").style.display = "inline";
+                document.getElementById("scroll-to-top").style.visibility = "visible";
+                document.getElementById("scroll-to-top").style.opacity = "1";
+            } else {
+                document.getElementById("scroll-to-top").style.visibility = "hidden";
+                document.getElementById("scroll-to-top").style.opacity = "0";
+            }
+            {{ end }}
+            prevScrollpos = currentScrollPos;
+        }
+        {{ if .Params.math }}
+            renderMathInElement(document.body,
+                {
+                    delimiters: [
+                        {left: "$$", right: "$$", display: true},
+                        {left: "$", right: "$", display: false},
+                    ]
+                }
+            );
+
+            var inlineMathArray = document.querySelectorAll("script[type='math/tex']");
+            for (var i = 0; i < inlineMathArray.length; i++) {
+                var inlineMath = inlineMathArray[i];
+                var tex = inlineMath.innerText || inlineMath.textContent;
+                var replaced = document.createElement("span");
+                replaced.innerHTML = katex.renderToString(tex, {displayMode: false});
+                inlineMath.parentNode.replaceChild(replaced, inlineMath);
+            }
+
+            var displayMathArray = document.querySelectorAll("script[type='math/tex; mode=display']");
+            for (var i = 0; i < displayMathArray.length; i++) {
+                var displayMath = displayMathArray[i];
+                var tex = displayMath.innerHTML;
+                var replaced = document.createElement("span");
+                replaced.innerHTML = katex.renderToString(tex.replace(/%.*/g, ''), {displayMode: true});
+                displayMath.parentNode.replaceChild(replaced, displayMath);
+            }
+        {{ end }}
+
+        {{ if .Site.Params.showtoc | default true }}
+        if (document.getElementById("contents-list") !== null && document.getElementsByClassName("post-content").length !== 0) {
+            tocbot.init({
+                // Where to render the table of contents.
+                tocSelector: '#contents-list',
+                // Where to grab the headings to build the table of contents.
+                contentSelector: '.post-content',
+                // Which headings to grab inside of the contentSelector element.
+                headingSelector: 'h1, h2, h3',
+            });
+        }
+        {{ end }}
+    });
+    {{ end }}
+})();
---
+++ assets/js/matomo.js
@@ -0,0 +1,15 @@
+(function(){
+    'use strict';
+    var _paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDomains", ["*.the.geekorium.com.au"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="https://visitors.geekorium.com.au/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+})();
